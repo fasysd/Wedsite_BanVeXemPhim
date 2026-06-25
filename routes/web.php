9e5c899cb877d204fe3b7a6f19b63fe42c469fe9
fasysd <manhhung25017@gmail.com>
2026-06-25 14:47:52 +0700

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,43 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\AdminMovieController;
 use App\Http\Controllers\AdminRoomController;
 use App\Http\Controllers\AdminShowtimeController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\AdminStaffController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\TicketController;
 
-Route::get('/login', function () {
-    return view('user.login');
-})->name('login');
+/*
+|--------------------------------------------------------------------------
+| AUTH
+|--------------------------------------------------------------------------
+*/
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+
+/*
+|--------------------------------------------------------------------------
+| PUBLIC
+|--------------------------------------------------------------------------
+*/
 Route::get('/', function () {
     return view('user.index');
 })->name('user.index');
+
+Route::get('/movies', [MovieController::class, 'index'])->name('movies');
+Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movie.show');
+
+Route::get('/movies/{movie}/booking', [TicketController::class, 'booking'])
+    ->middleware('auth')
+    ->name('ticket.booking');
+
 Route::get('/help/about', function () {
     return view('user.help.about');
 })->name('user.help.about');
@@ -20,45 +46,125 @@ Route::get('/welcome', function () {
     return view('home.index');
 });
 
-Route::view('/admin', 'admin')->name('admin.dashboard');
+/*
+|--------------------------------------------------------------------------
+| ACCOUNT (USER)
+|--------------------------------------------------------------------------
+*/
+Route::middleware('auth')->group(function () {
 
-Route::view('/admin/movies', 'admin.movies.index')->name('admin.movies.index');
+    Route::get('/account/general', [AccountController::class, 'show'])
+        ->name('user.account.general');
 
-Route::get('/admin/rooms', [AdminRoomController::class, 'index'])
-    ->name('admin.rooms.index');
-Route::get('/admin/rooms/create', [AdminRoomController::class, 'create'])
-    ->name('admin.rooms.create');
-Route::post('/admin/rooms', [AdminRoomController::class, 'store'])
-    ->name('admin.rooms.store');
-Route::get('/admin/rooms/{id}/edit', [AdminRoomController::class, 'edit'])
-    ->name('admin.rooms.edit');
-Route::get('/admin/rooms/{id}', [AdminRoomController::class, 'show'])
-    ->name('admin.rooms.show');
-Route::put('/admin/rooms/{id}', [AdminRoomController::class, 'update'])
-    ->name('admin.rooms.update');
-Route::delete('/admin/rooms/{id}', [AdminRoomController::class, 'destroy'])
-    ->name('admin.rooms.destroy');
+    Route::get('/account/detail', [AccountController::class, 'edit'])
+        ->name('user.account.detail');
 
-Route::get('/admin/showtimes', [AdminShowtimeController::class, 'index'])
-    ->name('admin.showtimes.index');
-Route::get('/admin/showtimes/create', [AdminShowtimeController::class, 'create'])
-    ->name('admin.showtimes.create');
-Route::post('/admin/showtimes', [AdminShowtimeController::class, 'store'])
-    ->name('admin.showtimes.store');
-Route::get('/admin/showtimes/{id}/edit', [AdminShowtimeController::class, 'edit'])
-    ->name('admin.showtimes.edit');
-Route::get('/admin/showtimes/{id}', [AdminShowtimeController::class, 'show'])
-    ->name('admin.showtimes.show');
-Route::put('/admin/showtimes/{id}', [AdminShowtimeController::class, 'update'])
-    ->name('admin.showtimes.update');
-Route::delete('/admin/showtimes/{id}', [AdminShowtimeController::class, 'destroy'])
-    ->name('admin.showtimes.destroy');
+    Route::put('/account/detail', [AccountController::class, 'update'])
+        ->name('user.account.detail.update');
 
+    Route::put('/account/password', [AccountController::class, 'resetPassword'])
+        ->name('user.account.password.update');
 
-        Route::get('/staff', [AdminStaffController::class, 'index'])->name('staff.index');
-        Route::get('/staff/create', [AdminStaffController::class, 'create'])->name('staff.create');
-        Route::post('/staff', [AdminStaffController::class, 'store'])->name('staff.store');
-        Route::get('/staff/{staff}/edit', [AdminStaffController::class, 'edit'])->name('staff.edit');
-        Route::put('/staff/{staff}', [AdminStaffController::class, 'update'])->name('staff.update');
-        Route::put('/staff/{staff}/reset-password', [AdminStaffController::class, 'resetPassword'])->name('staff.reset-password');
+    Route::get('/account/tickets', [TicketController::class, 'index'])
+        ->name('user.account.tickets');
+
+    Route::get('/account/tickets/{id}', [TicketController::class, 'show'])
+        ->name('user.account.tickets.show');
+});
+
+/*
+|--------------------------------------------------------------------------
+| STAFF
+|--------------------------------------------------------------------------
+*/
+
+/*
+|--------------------------------------------------------------------------
+| ADMIN
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth', 'admin'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+
+        /*
+        | Dashboard
+        */
+        Route::redirect('/', '/admin/rooms')->name('admin.dashboard');
+
+        /*
+        | Movies
+        */
+        Route::view('/movies', 'admin.movies.index')
+            ->name('movies.index');
+
+        /*
+        | Rooms
+        */
+        Route::get('/rooms', [AdminRoomController::class, 'index'])
+            ->name('rooms.index');
+
+        Route::get('/rooms/create', [AdminRoomController::class, 'create'])
+            ->name('rooms.create');
+
+        Route::post('/rooms', [AdminRoomController::class, 'store'])
+            ->name('rooms.store');
+
+        Route::get('/rooms/{id}/edit', [AdminRoomController::class, 'edit'])
+            ->name('rooms.edit');
+
+        Route::get('/rooms/{id}', [AdminRoomController::class, 'show'])
+            ->name('rooms.show');
+
+        Route::put('/rooms/{id}', [AdminRoomController::class, 'update'])
+            ->name('rooms.update');
+
+        Route::delete('/rooms/{id}', [AdminRoomController::class, 'destroy'])
+            ->name('rooms.destroy');
+
+        /*
+        | Showtimes
+        */
+        Route::get('/showtimes', [AdminShowtimeController::class, 'index'])
+            ->name('showtimes.index');
+
+        Route::get('/showtimes/create', [AdminShowtimeController::class, 'create'])
+            ->name('showtimes.create');
+
+        Route::post('/showtimes', [AdminShowtimeController::class, 'store'])
+            ->name('showtimes.store');
+
+        Route::get('/showtimes/{id}', [AdminShowtimeController::class, 'show'])
+            ->name('showtimes.show');
+
+        Route::get('/showtimes/{id}/edit', [AdminShowtimeController::class, 'edit'])
+            ->name('showtimes.edit');
+
+        Route::put('/showtimes/{id}', [AdminShowtimeController::class, 'update'])
+            ->name('showtimes.update');
+
+        Route::delete('/showtimes/{id}', [AdminShowtimeController::class, 'destroy'])
+            ->name('showtimes.destroy');
+
+        /*
+        | Staff
+        */
+        Route::get('/staff', [AdminStaffController::class, 'index'])
+            ->name('staff.index');
+
+        Route::get('/staff/create', [AdminStaffController::class, 'create'])
+            ->name('staff.create');
+
+        Route::post('/staff', [AdminStaffController::class, 'store'])
+            ->name('staff.store');
+
+        Route::get('/staff/{staff}/edit', [AdminStaffController::class, 'edit'])
+            ->name('staff.edit');
+
+        Route::put('/staff/{staff}', [AdminStaffController::class, 'update'])
+            ->name('staff.update');
+
+        Route::put('/staff/{staff}/reset-password', [AdminStaffController::class, 'reset-password'])
+            ->name('staff.reset-password');
     });
